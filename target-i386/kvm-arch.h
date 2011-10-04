55b583014235f8594888ec31152b452773d17e1d
Eduardo Habkost <ehabkost@redhat.com>
2011-10-04 17:30:18 -0300

    kvm: create struct kvm_arch_state
    We need to carry arch-speficic data on KVMState, and the data needs to
    be acessible to target-*/kvm.c, so define a new struct for that. Each
    target will have its own struct definition.
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

---
+++ target-i386/kvm-arch.h
@@ -0,0 +1,13 @@
+#ifndef _QEMU_I386_KVM_ARCH
+#define _QEMU_I386_KVM_ARCH
+
+/* arch-specific definitions for KVM for target-i386 */
+
+
+/** This struct carries arch-specific KVMState data for target-i386
+ */
+struct kvm_arch_state {
+    /* No arch-specific data by now */
+};
+
+#endif /* _QEMU_I386_KVM_ARCH */
