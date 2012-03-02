f6a9704389e48352730a8fb68182bb26ccd9baec
Eduardo Habkost <ehabkost@redhat.com>
2012-03-02 19:30:54 -0300

    target-i386/helper.c: set inited = 1 even without TCG
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- target-i386/helper.c
+++ target-i386/helper.c
@@ -1261,14 +1261,16 @@ CPUX86State *cpu_x86_init(const char *cpu_model)
     cpu_exec_init(env);
     env->cpu_model_str = cpu_model;
 
-    /* init various static tables used in TCG mode */
-    if (tcg_enabled() && !inited) {
+    if (!inited) {
         inited = 1;
-        optimize_flags_init();
+        /* init various static tables used in TCG mode */
+        if (tcg_enabled()) {
+            optimize_flags_init();
 #ifndef CONFIG_USER_ONLY
-        prev_debug_excp_handler =
-            cpu_set_debug_excp_handler(breakpoint_handler);
+            prev_debug_excp_handler =
+                cpu_set_debug_excp_handler(breakpoint_handler);
 #endif
+        }
     }
     if (cpu_x86_register(env, cpu_model) < 0) {
         cpu_x86_close(env);
