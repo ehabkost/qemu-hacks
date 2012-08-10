07acede5587b4d47361bc8d34951b54be3345b10
Igor Mammedov <imammedo@redhat.com>
2012-08-10 12:03:33 +0200

    target-i386: move default init of cpuid_kvm_features bitmap into CPU initializer from cpudef
    Moving it inside CPU initializer from cpudef will help to split
    cpu_x86_find_by_name() into default init and user settable properties.
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>

--- target-i386/cpu.c
+++ target-i386/cpu.c
@@ -1298,6 +1298,9 @@ static void cpudef_2_x86_cpu(X86CPU *cpu, x86_def_t *def, Error **errp)
     env->cpuid_7_0_ebx = def->cpuid_7_0_ebx_features;
     env->cpuid_xlevel2 = def->xlevel2;
 
+    /* not supported bits will be filtered out later */
+    env->cpuid_kvm_features = ~0;
+
     object_property_set_bool(OBJECT(cpu), true, "hypervisor", errp);
 }
 
@@ -1373,7 +1376,6 @@ static int cpu_x86_find_by_name(X86CPU *cpu, x86_def_t *x86_cpu_def,
 {
     x86_def_t *def;
 
-    CPUX86State *env = &cpu->env;
     QDict *features;
     char *name;
 
@@ -1394,9 +1396,6 @@ static int cpu_x86_find_by_name(X86CPU *cpu, x86_def_t *x86_cpu_def,
     }
 
     cpudef_2_x86_cpu(cpu, x86_cpu_def, errp);
-
-    /* not supported bits will be filtered out later */
-    env->cpuid_kvm_features = ~0;
 
     cpu_x86_set_props(cpu, features, errp);
     QDECREF(features);
