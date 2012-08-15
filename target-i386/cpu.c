0f5b4dd9d1ca10cd271bc5bc5117f47d48c7f546
Eduardo Habkost <ehabkost@redhat.com>
2012-08-15 11:09:17 -0300

    HACK to initialize types later
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- target-i386/cpu.c
+++ target-i386/cpu.c
@@ -1639,6 +1639,8 @@ void cpu_clear_apic_feature(CPUX86State *env)
 
 #endif /* !CONFIG_USER_ONLY */
 
+static void x86_cpu_register_types(void);
+
 /* register "cpudef" models defined in configuration file.  Here we first
  * preload any built-in definitions
  */
@@ -1669,6 +1671,8 @@ void x86_cpudef_setup(void)
 #if !defined(CONFIG_USER_ONLY)
     qemu_opts_foreach(qemu_find_opts("cpudef"), cpudef_register, NULL, 0);
 #endif
+
+    x86_cpu_register_types();
 }
 
 static void get_cpuid_vendor(CPUX86State *env, uint32_t *ebx,
@@ -2258,4 +2262,5 @@ static void x86_cpu_register_types(void)
 
 }
 
-type_init(x86_cpu_register_types)
+//HACK: the function is being called from x86_cpudef_setup()
+//type_init(x86_cpu_register_types)
