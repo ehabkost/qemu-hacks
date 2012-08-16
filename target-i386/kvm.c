c8da6df6b41b7dd0d531d63e70ce64d9cddf8997
Eduardo Habkost <ehabkost@redhat.com>
2012-08-16 10:36:26 -0300

    set CPUID_EXT_HYPERVISOR on kvm_arch_get_supported_cpuid()
    Full grep for kvm_arch_get_supported_cpuid:
    
       kvm.h:uint32_t kvm_arch_get_supported_cpuid(KVMState *env, uint32_t function,
       target-i386/cpu.c:                kvm_arch_get_supported_cpuid(kvm_state, 0x7, 0, R_EBX);
       target-i386/cpu.c:            *eax = kvm_arch_get_supported_cpuid(s, 0xA, count, R_EAX);
       target-i386/cpu.c:            *ebx = kvm_arch_get_supported_cpuid(s, 0xA, count, R_EBX);
       target-i386/cpu.c:            *ecx = kvm_arch_get_supported_cpuid(s, 0xA, count, R_ECX);
       target-i386/cpu.c:            *edx = kvm_arch_get_supported_cpuid(s, 0xA, count, R_EDX);
       target-i386/cpu.c:            *eax = kvm_arch_get_supported_cpuid(s, 0xd, count, R_EAX);
       target-i386/cpu.c:            *ebx = kvm_arch_get_supported_cpuid(s, 0xd, count, R_EBX);
       target-i386/cpu.c:            *ecx = kvm_arch_get_supported_cpuid(s, 0xd, count, R_ECX);
       target-i386/cpu.c:            *edx = kvm_arch_get_supported_cpuid(s, 0xd, count, R_EDX);
       target-i386/kvm.c:uint32_t kvm_arch_get_supported_cpuid(KVMState *s, uint32_t function,
       target-i386/kvm.c:        cpuid_1_edx = kvm_arch_get_supported_cpuid(s, 1, 0, R_EDX);
       target-i386/kvm.c:            kvm_arch_get_supported_cpuid(s, 1, 0, R_EDX);
     * target-i386/kvm.c:            kvm_arch_get_supported_cpuid(s, 1, 0, R_ECX);
       target-i386/kvm.c:            kvm_arch_get_supported_cpuid(s, 0x80000001,
       target-i386/kvm.c:            kvm_arch_get_supported_cpuid(s, 0x80000001,
       target-i386/kvm.c:            kvm_arch_get_supported_cpuid(s, 0x8000000A,
       target-i386/kvm.c:        kvm_arch_get_supported_cpuid(s, KVM_CPUID_FEATURES, 0, R_EAX);
       target-i386/kvm.c:            kvm_arch_get_supported_cpuid(s, 0xC0000001, 0, R_EDX);
    
    Note that there is only one call for CPUID[1].ECX above, and it is the one that
    gets hacked to include CPUID_EXT_HYPERVISOR, so we can simply make
    kvm_arch_get_supported_cpuid() set it, to let the rest of the code know the
    flag can be set by QEMU.
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- target-i386/kvm.c
+++ target-i386/kvm.c
@@ -198,6 +198,9 @@ uint32_t kvm_arch_get_supported_cpuid(KVMState *s, uint32_t function,
     if (function == 1 && reg == R_EDX) {
         /* KVM before 2.6.30 misreports the following features */
         ret |= CPUID_MTRR | CPUID_PAT | CPUID_MCE | CPUID_MCA;
+    } else if (function == 1 && reg == R_ECX) {
+        /* We set this flag, even if KVM does not return it */
+        ret |= CPUID_EXT_HYPERVISOR;
     } else if (function == 0x80000001 && reg == R_EDX) {
         /* On Intel, kvm returns cpuid according to the Intel spec,
          * so add missing bits according to the AMD spec:
@@ -400,11 +403,9 @@ int kvm_arch_init_vcpu(CPUX86State *env)
     env->feature_words[CPUID_1_EDX] &=
             kvm_arch_get_supported_cpuid(s, 1, 0, R_EDX);
 
-    i = env->feature_words[CPUID_1_ECX] & CPUID_EXT_HYPERVISOR;
     j = env->feature_words[CPUID_1_ECX] & CPUID_EXT_TSC_DEADLINE_TIMER;
     env->feature_words[CPUID_1_ECX] &=
             kvm_arch_get_supported_cpuid(s, 1, 0, R_ECX);
-    env->feature_words[CPUID_1_ECX] |= i;
     if (j && kvm_irqchip_in_kernel() &&
         kvm_check_extension(s, KVM_CAP_TSC_DEADLINE_TIMER)) {
         env->feature_words[CPUID_1_ECX] |= CPUID_EXT_TSC_DEADLINE_TIMER;
