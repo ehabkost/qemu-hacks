e177961f348333b405fd775e1d2e0fa4e4b9a2bb
Eduardo Habkost <ehabkost@redhat.com>
2012-08-16 13:04:39 -0300

    i386: coding style fixes
    Just fix some 'if' statements without braces.
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- target-i386/helper.c
+++ target-i386/helper.c
@@ -448,12 +448,14 @@ void cpu_x86_update_cr4(CPUX86State *env, uint32_t new_cr4)
         tlb_flush(env, 1);
     }
     /* SSE handling */
-    if (!(env->cpuid_features & CPUID_SSE))
+    if (!(env->cpuid_features & CPUID_SSE)) {
         new_cr4 &= ~CR4_OSFXSR_MASK;
-    if (new_cr4 & CR4_OSFXSR_MASK)
+    }
+    if (new_cr4 & CR4_OSFXSR_MASK) {
         env->hflags |= HF_OSFXSR_MASK;
-    else
+    } else {
         env->hflags &= ~HF_OSFXSR_MASK;
+    }
 
     env->cr[4] = new_cr4;
 }
