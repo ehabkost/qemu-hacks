1f308916bfbfc39954f8bc5cf2bd4e859e7d7d52
Eduardo Habkost <ehabkost@redhat.com>
2012-08-16 14:24:14 -0300

    rename cpu_x86_init() to cpu_x86_create()
    Just to make it clear that the function not just initializes a CPU
    object, but creates a new one.
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- hw/pc.c
+++ hw/pc.c
@@ -911,7 +911,7 @@ static X86CPU *pc_new_cpu(const char *cpu_model)
     X86CPU *cpu;
     CPUX86State *env;
 
-    cpu = cpu_x86_init(cpu_model);
+    cpu = cpu_x86_create(cpu_model);
     if (cpu == NULL) {
         fprintf(stderr, "Unable to find x86 CPU definition\n");
         exit(1);
