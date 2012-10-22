d7f57a46d07c0a72295a56704ab0fecefb2aaea8
Igor Mammedov <imammedo@redhat.com>
2012-10-22 17:03:00 +0200

    target-i386: Postpone cpuid_level update to realize time
    Delay capping cpuid_level to 7 to realize time so property setters
    for cpuid_7_0_ebx_features and "level" could be used in any order/time
    between x86_cpu_initfn() and x86_cpu_realize().
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Reviewed-by: Eduardo Habkost <ehabkost@redhat.com>
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- target-i386/cpu.c
+++ target-i386/cpu.c
@@ -1381,9 +1381,6 @@ static int cpu_x86_parse_featurestr(x86_def_t *x86_cpu_def, char *features)
         if (kvm_check_features_against_host(x86_cpu_def) && enforce_cpuid)
             goto error;
     }
-    if (x86_cpu_def->cpuid_7_0_ebx_features && x86_cpu_def->level < 7) {
-        x86_cpu_def->level = 7;
-    }
     return 0;
 
 error:
