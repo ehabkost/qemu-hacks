6d26f7eb6732111474504659cade491af1c51565
Eduardo Habkost <ehabkost@redhat.com>
2012-10-22 18:56:39 -0200

    pc: pass PCInitArgs struct to pc_bios_init()
    The argument will be used later, when enabling compatibility mode for
    the APIC ID generation code.
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- hw/pc.c
+++ hw/pc.c
@@ -573,7 +573,7 @@ int e820_add_entry(uint64_t address, uint64_t length, uint32_t type)
     return index;
 }
 
-static FWCfgState *pc_bios_init(void)
+static FWCfgState *pc_bios_init(PCInitArgs *args)
 {
     FWCfgState *fw_cfg;
     uint8_t *smbios_table;
@@ -945,7 +945,7 @@ FWCfgState *pc_memory_init(PCInitArgs *args)
                                         option_rom_mr,
                                         1);
 
-    fw_cfg = pc_bios_init();
+    fw_cfg = pc_bios_init(args);
     rom_set_fw(fw_cfg);
 
     if (linux_boot) {
