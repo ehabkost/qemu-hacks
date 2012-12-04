stamhe/qemu

target-i386: use visit_type_hz to parse tsc_freq property value
Signed-off-by: Igor Mammedov <imammedo@redhat.com>

target-i386/cpu.c
@@ -952,7 +952,7 @@ static void x86_cpuid_set_tsc_freq(Object *obj, Visitor *v, void *opaque,
     const int64_t max = INT_MAX;
     int64_t value;
 
-    visit_type_int(v, &value, name, errp);
+    visit_type_hz(v, &value, name, errp);
     if (error_is_set(errp)) {
         return;
     }

ehabkost/qemu-hacks

Reviewed-by: Andreas Färber <afaerber@suse.de>

target-i386/cpu.c
@@ -1195,7 +1195,7 @@ static void x86_cpuid_set_tsc_freq(Object *obj, Visitor *v, void *opaque,
     const int64_t max = INT64_MAX;
     int64_t value;
 
-    visit_type_int(v, &value, name, errp);
+    visit_type_freq(v, &value, name, errp);
     if (error_is_set(errp)) {
         return;
     }