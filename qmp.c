fade1dd8e5cd73fb1be01cc449c9620e5b994cc2
Igor Mammedov <imammedo@redhat.com>
2014-03-26 10:36:59 +0000

    qmp: allow object-add completion handler to get canonical path
    Add object to /objects before calling user_creatable_complete()
    handler, so that object might be able to call
    object_get_canonical_path() in its completion handler.
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- qmp.c
+++ qmp.c
@@ -571,13 +571,15 @@ void object_add(const char *type, const char *id, const QDict *qdict,
         }
     }
 
-    user_creatable_complete(obj, &local_err);
-    if (local_err) {
-        goto out;
-    }
-
     object_property_add_child(container_get(object_get_root(), "/objects"),
                               id, obj, &local_err);
+
+    user_creatable_complete(obj, &local_err);
+    if (local_err) {
+        object_property_del(container_get(object_get_root(), "/objects"),
+                            id, &error_abort);
+        goto out;
+    }
 out:
     if (local_err) {
         error_propagate(errp, local_err);
