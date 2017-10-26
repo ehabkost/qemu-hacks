4e8f85f78b3ed19092b3cb4737e0fd2e9b3d1cdb
Eduardo Habkost <ehabkost@redhat.com>
2017-10-26 16:46:59 +0200

    qemu/cpu: x-force-features option
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- target/i386/cpu.h
+++ target/i386/cpu.h
@@ -1384,6 +1384,12 @@ struct X86CPU {
     bool hyperv_tlbflush;
     bool check_cpuid;
     bool enforce_cpuid;
+    /*
+     * Force features to be enabled even if the host doesn't support them.
+     * This is dangerous and should be done only for testing CPUID
+     * compatibility.
+     */
+    bool force_features;
     bool expose_kvm;
     bool expose_tcg;
     bool migratable;
