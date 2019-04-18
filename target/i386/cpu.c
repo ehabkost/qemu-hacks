f6e53bdbd44c01f09757d0e99c82d029858162ba
Eduardo Habkost <ehabkost@redhat.com>
2019-04-18 01:52:24 -0300

    x86: set class name format
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -1391,15 +1391,6 @@ static char *x86_cpu_type_name(const char *model_name)
     return g_strdup_printf(X86_CPU_TYPE_NAME("%s"), model_name);
 }
 
-static ObjectClass *x86_cpu_class_by_name(const char *cpu_model)
-{
-    ObjectClass *oc;
-    char *typename = x86_cpu_type_name(cpu_model);
-    oc = object_class_by_name(typename);
-    g_free(typename);
-    return oc;
-}
-
 static char *x86_cpu_class_get_model_name(X86CPUClass *cc)
 {
     const char *class_name = object_class_get_name(OBJECT_CLASS(cc));
@@ -5851,7 +5842,7 @@ static void x86_cpu_common_class_init(ObjectClass *oc, void *data)
     cc->reset = x86_cpu_reset;
     cc->reset_dump_flags = CPU_DUMP_FPU | CPU_DUMP_CCOP;
 
-    cc->class_by_name = x86_cpu_class_by_name;
+    cc->class_name_format = X86_CPU_TYPE_NAME("%s");
     cc->parse_features = x86_cpu_parse_featurestr;
     cc->has_work = x86_cpu_has_work;
 #ifdef CONFIG_TCG
