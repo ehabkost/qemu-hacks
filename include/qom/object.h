e85a5482d40578635c86c0a5cb1894bf923eaee1
Eduardo Habkost <ehabkost@redhat.com>
2020-09-04 14:54:30 -0400

    qom: Remove function prototypes from OBJECT_DEFINE_TYPE_EXTENDED
    We don't need to force all users of OBJECT_DEFINE_* to implement
    all init/finalize functions.  We can just let them set the
    .instance_init/.instance_finalize/.class_init fields when
    necessary.
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- include/qom/object.h
+++ include/qom/object.h
@@ -328,13 +328,8 @@ struct Object
  *
  * This macro is typically used in a source file, and will:
  *
- *   - declare prototypes for _finalize, _class_init and _init methods
  *   - declare the TypeInfo struct instance
  *   - provide the constructor to register the type
- *
- * After using this macro, implementations of the _finalize, _class_init,
- * and _init methods need to be written. Any of these can be zero-line
- * no-op impls if no special logic is required for a given type.
  *
  * This macro should rarely be used, instead one of the more specialized
  * macros is usually a better choice.
@@ -342,22 +337,13 @@ struct Object
 #define OBJECT_DEFINE_TYPE_EXTENDED(ModuleObjName, module_obj_name, \
                                     MODULE_OBJ_NAME, PARENT_MODULE_OBJ_NAME, \
                                     ...) \
-    static void \
-    module_obj_name##_finalize(Object *obj); \
-    static void \
-    module_obj_name##_class_init(ObjectClass *oc, void *data); \
-    static void \
-    module_obj_name##_init(Object *obj); \
-    \
     static const TypeInfo module_obj_name##_info = { \
         .parent = TYPE_##PARENT_MODULE_OBJ_NAME, \
         .name = TYPE_##MODULE_OBJ_NAME, \
         .instance_size = sizeof(ModuleObjName), \
         .instance_align = __alignof__(ModuleObjName), \
-        .instance_init = module_obj_name##_init, \
         .instance_finalize = module_obj_name##_finalize, \
         .class_size = sizeof(ModuleObjName##Class), \
-        .class_init = module_obj_name##_class_init, \
         __VA_ARGS__ \
     }; \
     \
