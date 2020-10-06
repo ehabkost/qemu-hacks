4abc55d335ea708f4cc5baf5106c529882f5e01b
Eduardo Habkost <ehabkost@redhat.com>
2020-10-06 17:11:01 -0400

    hw/riscv: Use object_property_add_path_alias()
    Apply scripts/coccinelle/qom-use-object_property_add_path_alias.cocci
    to hw/riscv.
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- hw/riscv/virt.c
+++ hw/riscv/virt.c
@@ -91,8 +91,7 @@ static PFlashCFI01 *virt_flash_create1(RISCVVirtState *s,
     qdev_prop_set_string(dev, "name", name);
 
     object_property_add_child(OBJECT(s), name, OBJECT(dev));
-    object_property_add_alias(OBJECT(s), alias_prop_name,
-                              OBJECT(dev), "drive");
+    object_property_add_path_alias(OBJECT(s), alias_prop_name, name, "drive");
 
     return PFLASH_CFI01(dev);
 }
